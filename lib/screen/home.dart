9edf08880e73ae701638e78dd4f05f762de65917
kwonkangbin <kbk282655@gmail.com>
2023-06-14 01:40:05 +0900

    home

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -24,9 +24,28 @@ class _HomeScreenState extends State<HomeScreen> {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Center(child: Text('이미지'),)
+          Center(child: Text('이미지')),
+          Padding(
+            padding: EdgeInsets.all(width * 0.048),
+          ),
+          Container(
+            padding: EdgeInsets.only(bottom: width*0.036),
+            child: Center(
+              child: ButtonTheme(
+                minWidth: width * 0.8,
+                height: height * 0.05,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: ElevatedButton(
+                  child: Text('퀴즈풀기'),
+                  onPressed: (){},
+                ),
+              ),
+            ),
+          )
         ],
       ),
-    ))
+    ));
   }
 }
